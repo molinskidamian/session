97d3fc795433cb3ff9a4e4c45e2534ef92b4e148
Damian Molinski <tedeer@gmail.com>
2022-04-25 23:22:05 +0200

    Started work with class User

--- class/User.class.php
+++ class/User.class.php
@@ -1,4 +1,29 @@
 <?php
 class User extends Session{
+    private $firstName;
+    private $lastName;
+    private $email;
+
+    public function setFirstName($firstName){
+        $this->firstName = $firstName;
+    }
+
+    public function setLastName($lastName){
+        $this->lastName = $lastName;
+    }
+
+    public function setEmail($email){
+        $this->email = $email;
+    }
+
+    public function getFirstName(){
+        return $this->firstName;
+    }
+    public function getLastName(){
+        return $this->lastName;
+    }
+    public function getEmail(){
+        return $this->email;
+    }
 
 }
